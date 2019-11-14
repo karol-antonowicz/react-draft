d1f2b429ba5488f58b59c8cc4d0815686ae6500c
Kacper Grzeszczyk <kacgrzes@gmail.com>
2019-11-14 13:04:26 +0100

    Add logic (state) to Counter component

--- workshops/src/components/Counter.js
+++ workshops/src/components/Counter.js
@@ -16,14 +16,30 @@ const increment = () => {
 };
 
 class Counter extends Component {
+  state = {
+    count: 10
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
   render() {
     return (
       <div className={styles.container}>
-        <button className={styles.button} onClick={decrement}>
+        <button className={styles.button} onClick={this.decrement}>
           -1
         </button>
-        <span className={styles.display}>{value}</span>
-        <button className={styles.button} onClick={increment}>
+        <span className={styles.display}>{this.state.count}</span>
+        <button className={styles.button} onClick={this.increment}>
           +1
         </button>
       </div>
