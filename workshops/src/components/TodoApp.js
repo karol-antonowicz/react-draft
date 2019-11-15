d3d9b62a6d8681f81381f4074d3a36a47c1edc0e
Kacper Grzeszczyk <kacgrzes@gmail.com>
2019-11-15 15:01:25 +0100

    Fix TodoApp errors

--- workshops/src/components/TodoApp.js
+++ workshops/src/components/TodoApp.js
@@ -52,14 +52,23 @@ const TodoItem = props => {
   return (
     <li className={isDone ? styles.completed : ""}>
       <div className={styles.view}>
-        <input className={styles.toggle} type="checkbox" checked={isDone} />
+        <input
+          className={styles.toggle}
+          type="checkbox"
+          checked={isDone}
+          readOnly
+        />
         <label>{label}</label>
         <button
           className={styles.destroy}
           onClick={() => props.onDeleteTodo(id)}
         ></button>
       </div>
-      <input className={styles.edit} value="Create a TodoMVC template" />
+      <input
+        className={styles.edit}
+        value="Create a TodoMVC template"
+        readOnly
+      />
     </li>
   );
 };
@@ -88,7 +97,7 @@ const TodoInput = props => {
       onChange={event => {
         props.onValueChange(event.target.value);
       }}
-      autofocus
+      autoFocus
     />
   );
 };
